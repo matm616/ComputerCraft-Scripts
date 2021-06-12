27a37b5174431f23edd6a79459752b7b927bff1e
matm616 <35334792+matm616@users.noreply.github.com>
2021-06-12 21:08:40 +0100

    Quick refactor to use camelCase

--- fetch.lua
+++ fetch.lua
@@ -1,27 +1,28 @@
 -- script to fetch all scrripts from my github repo
 -- written by matm616
 
+
+-- gets JSON API written by ElvishJerricco
 if not (fs.exists("/json.lua")) then
   shell.run("pastebin get 4nRg9CHU json.lua")
 end
 
 os.loadAPI("json.lua")
+githubLink = "https://api.github.com/repos/matm616/ComputerCraft-Scripts/contents"
 
-github_link = "https://api.github.com/repos/matm616/ComputerCraft-Scripts/contents"
-
-local github_handle = http.get(github_link)
-if not github_handle then
+local githubHandle = http.get(githubLink)
+if not githubHandle then
   print("Oops! I can't access the github api.")
 else
-  local content = github_handle.readAll()
-  github_handle.close()
+  local content = githubHandle.readAll()
+  githubHandle.close()
   
   obj = json.decode(content)
   for key,value in pairs( obj ) do
     if not (value.path == "readme.md") then
-      local repo_file = http.get(value.url).readAll()
+      local repoFile = http.get(value.url).readAll()
       f = fs.open("/matm/" .. value.path, "w")
-      f.write(repo_file)
+      f.write(repoFile)
       f.close()
       print()
     end
