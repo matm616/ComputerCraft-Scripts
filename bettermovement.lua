1bb17dac475ab9293ad2f8d275766cdea2b6aaf6
matm616 <35334792+matm616@users.noreply.github.com>
2021-06-12 19:10:31 +0100

    Added betermovement.lua

---
+++ bettermovement.lua
@@ -0,0 +1,39 @@
+-- BetterMovement
+-- A better (and incomplete) turtle movement API
+-- by matm616
+
+function forward()
+end
+
+function up()
+end
+
+function down()
+end
+
+function right()
+end
+
+function left()
+end
+
+function u()
+end
+
+function moveTo(x, y, z)
+end
+
+function checkpoint()
+end
+
+function lastCheckpoint()
+end
+
+function setHeading(direction)
+end
+
+function saveToFile()
+end
+
+function loadFromFile()
+end
