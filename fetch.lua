c2d50e0da1bf4d88caba60fc1d81528c733cbd21
matm616 <35334792+matm616@users.noreply.github.com>
2021-06-13 18:01:45 +0100

    Fixed issue with wrong URL and bad http header
    Using the "download_url" from github api instead of "url"
    Fixed misunderstanding with auth api header, now formatted correctly

--- fetch.lua
+++ fetch.lua
@@ -1,7 +1,6 @@
--- script to fetch all scrripts from my github repo
--- written by matm616
+-- Script to fetch all scripts from my github repo
+-- Written by matm616
 -- To use authentication, place a file within /githubToken/ named the github user account and containing a personal access token
-
 
 -- gets JSON API written by ElvishJerricco
 if not (fs.exists("/json.lua")) then
@@ -28,8 +27,7 @@ if (auth == false) then
   print("Accessing GitHub API without authentication. Beware of the rate limit! (60 requests per hour)")
   githubHandle = http.get(githubLink)
 else
-  headers = {}
-  headers["Authorization: Basic"] = user .. ":" .. token
+  headers = {user .. ":" .. token}
   print("Using " .. user .. " for API")
   githubHandle = http.get(githubLink, headers)
 end
@@ -44,7 +42,7 @@ else
   for key,value in pairs( obj ) do
     if not (value.path == "readme.md") then
       print("Getting " .. value.path .. "...")
-      local repoFile = http.get(value.url).readAll()
+      local repoFile = http.get(value.download_url).readAll()
       f = fs.open("/matm/" .. value.path, "w")
       f.write(repoFile)
       f.close()
