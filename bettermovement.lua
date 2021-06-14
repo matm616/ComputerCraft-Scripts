f014a7a917d3fba94bcd3c2e5ef7947a577c0a7a
matm616 <35334792+matm616@users.noreply.github.com>
2021-06-15 00:41:53 +0100

    Added *untested* loading from file
    Script will check if files exists
    Will then unserialise them into memory

--- bettermovement.lua
+++ bettermovement.lua
@@ -2,6 +2,18 @@
 -- A better (and incomplete) turtle movement API
 -- by matm616
 
+
+-- Variables
+location = {
+    ["currentX"] = 0,
+    ["currentY"] = 0,
+    ["currentZ"] = 0,
+    ["currentHeading"] = 0,
+}
+
+waypoints = {}
+
+-- Functions
 function forward()
 end
 
@@ -35,5 +47,25 @@ end
 function saveToFile()
 end
 
+
+-- untested load from file
+-- loads what should be the current location and any left over previous waypoints
 function loadFromFile()
+    -- if /BetterMovement dir exists
+    if (fs.exists("/BetterMovement")) then
+        -- if location file exists
+        if (fs.exists("/BetterMovement/location")) then
+            location = {} -- empty location var
+            varFile = fs.open("/BetterMovement/location", "r") -- open file
+            location = textutils.unserialise(varFile.readAll()) -- unserialise data into var
+            varFile.close() -- close file
+        end
+        -- if waypoint file exists
+        if (fs.exists("/BetterMovement/waypoints")) then
+            waypoints = {} -- empty waypoints var
+            varFile = fs.open("/BetterMovement/waypoints", "r") -- open file
+            waypoints = textutils.unserialise(varFile.readAll()) -- unserialise data into var
+            varFile.close() -- close file
+        end
+    end
 end
